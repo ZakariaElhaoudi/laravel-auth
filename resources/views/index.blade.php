5cfeb02ef14a7f4ee127ae8ab3af34224759b665
Zakaria96 <zakarialavoro@gmail.com>
2023-07-20 16:35:20 +0200

    added index and show

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container ">
+        <div class="row">
+            <div class="col">
+                <h1>All Projects</h1>
+                <ul>
+                    @foreach( $projects as $project)
+
+                        <a href="{{ route('show', $project -> id) }}">
+                            <li>{{ $project -> city}}</li>
+                        </a>
+            
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,5 +29,15 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route :: get("/", [ProjectController :: class, "index"])
+    -> name('index');
+
+Route :: get("/show{id}", [ProjectController :: class, "show"])
+    -> name('show');
 
 require __DIR__.'/auth.php';
